02fb05372519601d9f93a3e749c2d4f7319a032f
Tobi Tselios <tobi.tselios@gmail.com>
2021-01-19 01:35:10 +0000

    Initial redux setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
 import App from "./App";
-import "./index.css";
+import store from "./redux/store";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>,
+    document.getElementById("root")
+);
