9e685ec280acbec09564d0b5047fb10e25a82aea
Tobi Tselios <tobi.tselios@gmail.com>
2021-01-19 20:37:45 +0000

    General styles and small changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Login from "./pages/Login";
+import "./styles/main.scss";
 
 const App = () => {
-    return <div></div>;
+    return (
+        <Switch>
+            <Route path="/login" component={Login} />
+        </Switch>
+    );
 };
 
 export default App;
