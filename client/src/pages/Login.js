41024d5689770c75d49406d0fa2ea7f8b52fe2b3
Tobi Tselios <tobi.tselios@gmail.com>
2021-01-19 20:40:21 +0000

    Login page

---
+++ client/src/pages/Login.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import axiosInstance from "../api/auth";
+
+const initialForm = {
+    email: "",
+    password: ""
+};
+
+const Login = () => {
+    const [form, setForm] = useState(initialForm);
+
+    const handleChanges = event => {
+        setForm({ ...form, [event.target.name]: event.target.value });
+    };
+
+    const handleSubmit = async event => {
+        event.preventDefault();
+
+        try {
+            const { data } = await axiosInstance.post("/login", form);
+            localStorage.setItem("token", data.token);
+            setForm(initialForm);
+        } catch (error) {
+            console.log("LOGIN", error);
+        }
+    };
+
+    return (
+        <div className="login" onSubmit={handleSubmit}>
+            <form className="login__form">
+                <h2 className="login__form--heading">Sign In</h2>
+                <input
+                    name="email"
+                    type="text"
+                    value={form.email}
+                    onChange={handleChanges}
+                    placeholder="Email"
+                    className="login__form--input"
+                />
+                <input
+                    name="password"
+                    type="password"
+                    value={form.password}
+                    onChange={handleChanges}
+                    className="login__form--input"
+                    placeholder="Password"
+                />
+                <button className="login__form--btn" type="submit">
+                    Sign In
+                </button>
+                <p className="login__redirect">
+                    New to PressPlay?{" "}
+                    <span>
+                        <Link>Register</Link>
+                    </span>
+                </p>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
