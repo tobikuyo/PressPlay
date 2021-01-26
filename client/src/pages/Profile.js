b4e5dd804cb427cde10216b9e3213d9e86352ea6
Tobi Tselios <tobi.tselios@gmail.com>
2021-01-26 00:47:10 +0000

    Add sign out and fix text for empty watchlist

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 import { getAllMovies } from "../redux/actions/profileActions";
 import { IMAGE_URL } from "../constants/url";
 
@@ -12,32 +13,35 @@ const Profile = () => {
         dispatch(getAllMovies());
     }, [dispatch]);
 
-    const poster = movie => {
-        if (!movieList.length > 0) {
-            return <p className="profile__text">You have no saved movies</p>;
-        } else {
-            return (
-                <Link
-                    to={`/movie/${movie._id}`}
-                    className="profile__link"
-                    key={movie._id}
-                >
-                    <img
-                        src={`${IMAGE_URL}${movie.poster}`}
-                        alt={movie.title}
-                        className="profile__movie"
-                    />
-                </Link>
-            );
-        }
-    };
-
     return (
         <div className="profile">
+            <Link
+                to="/login"
+                className="profile__logout"
+                onClick={() => localStorage.removeItem("token")}
+            >
+                <ExitToAppIcon />
+            </Link>
             <h2 className="profile__name">My Watchlist</h2>
-            <div className="profile__container">
-                {movieList.map(movie => poster(movie))}
-            </div>
+            {!movieList.length > 0 ? (
+                <p className="profile__text">You have no saved movies</p>
+            ) : (
+                <div className="profile__container">
+                    {movieList.map(movie => (
+                        <Link
+                            to={`/movie/${movie._id}`}
+                            className="profile__link"
+                            key={movie._id}
+                        >
+                            <img
+                                src={`${IMAGE_URL}${movie.poster}`}
+                                alt={movie.title}
+                                className="profile__movie"
+                            />
+                        </Link>
+                    ))}
+                </div>
+            )}
         </div>
     );
 };
