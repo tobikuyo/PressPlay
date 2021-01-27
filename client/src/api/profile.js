17dac546c65c43aaa655fd7da809cef60e78a089
Tobi Tselios <tobi.tselios@gmail.com>
2021-01-27 00:52:11 +0000

    Deployed backend and change favicon

--- client/src/api/profile.js
+++ client/src/api/profile.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:8000/api/movies"
+    baseURL: "https://press-play-movies.herokuapp.com/api/movies"
 });
 
 export default instance;
