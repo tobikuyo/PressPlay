2de5e8517780216083e603aad51756e9067f404c
Tobi Tselios <tobi.tselios@gmail.com>
2021-01-30 23:23:02 +0000

    Edit SearchResults page

--- client/src/pages/SearchResults.js
+++ client/src/pages/SearchResults.js
@@ -9,24 +9,28 @@ const SearchResults = () => {
     return (
         <div className="results">
             <h2 className="results__title">Search Results</h2>
-            <div className="results__container">
-                {results.map(
-                    movie =>
-                        movie.poster_path && (
-                            <Link
-                                to={`/movie/${movie.id}`}
-                                className="results__link"
-                                key={movie.id}
-                            >
-                                <img
-                                    src={`${POSTER_URL}${movie.poster_path}`}
-                                    alt={movie.title}
-                                    className="results__movie"
-                                />
-                            </Link>
-                        )
-                )}
-            </div>
+            {!results.length > 0 ? (
+                <p className="results__text">No results found</p>
+            ) : (
+                <div className="results__container">
+                    {results.map(
+                        movie =>
+                            movie.poster_path && (
+                                <Link
+                                    to={`/movie/${movie.id}`}
+                                    className="results__link"
+                                    key={movie.id}
+                                >
+                                    <img
+                                        src={`${POSTER_URL}${movie.poster_path}`}
+                                        alt={movie.title}
+                                        className="results__movie"
+                                    />
+                                </Link>
+                            )
+                    )}
+                </div>
+            )}
         </div>
     );
 };
